f8ee77c77985b770dcfa3a8a3cd1e46dc00df39c
Kunal Batham <kunal.batham_cs20@gla.ac.in>
2022-07-18 13:59:11 +0530

    commit

---
+++ wwwroot/extensions/SummaryExtension.js
@@ -0,0 +1,95 @@
+import { BaseExtension } from "./BaseExtension.js";
+import { SummaryPanel } from "./SummaryPanel.js";
+
+class SummaryExtension extends BaseExtension {
+  constructor(viewer, options) {
+    super(viewer, options);
+    this._button = null;
+    this._panel = null;
+  }
+
+  load() {
+    super.load();
+    console.log("SummaryExtension loaded.");
+    return true;
+  }
+
+  unload() {
+    super.unload();
+    if (this._button) {
+      this.removeToolbarButton(this._button);
+      this._button = null;
+    }
+    if (this._panel) {
+      this._panel.setVisible(false);
+      this._panel.uninitialize();
+      this._panel = null;
+    }
+    console.log("SummaryExtension unloaded.");
+    return true;
+  }
+
+  onToolbarCreated() {
+    this._panel = new SummaryPanel(
+      this,
+      "model-summary-panel",
+      "Model Summary"
+    );
+    this._button = this.createToolbarButton(
+      "summary-button",
+      "https://img.icons8.com/small/32/brief.png",
+      "Show Model Summary"
+    );
+    this._button.onClick = () => {
+      // TODO
+      this._panel.setVisible(!this._panel.isVisible());
+      this._button.setState(
+        this._panel.isVisible()
+          ? Autodesk.Viewing.UI.Button.State.ACTIVE
+          : Autodesk.Viewing.UI.Button.State.INACTIVE
+      );
+      if (this._panel.isVisible()) {
+        this.update();
+      }
+    };
+  }
+
+  onModelLoaded(model) {
+    super.onModelLoaded(model);
+    this.update();
+  }
+
+  onSelectionChanged(model, dbids) {
+    super.onSelectionChanged(model, dbids);
+    this.update();
+  }
+
+  onIsolationChanged(model, dbids) {
+    super.onIsolationChanged(model, dbids);
+    this.update();
+  }
+
+  async update() {
+    // TODO
+    if (this._panel) {
+      const selectedIds = this.viewer.getSelection();
+      const isolatedIds = this.viewer.getIsolatedNodes();
+      if (selectedIds.length > 0) {
+        // If any nodes are selected, compute the aggregates for them
+        this._panel.update(this.viewer.model, selectedIds, SUMMARY_PROPS);
+      } else if (isolatedIds.length > 0) {
+        // Or, if any nodes are isolated, compute the aggregates for those
+        this._panel.update(this.viewer.model, isolatedIds, SUMMARY_PROPS);
+      } else {
+        // Otherwise compute the aggregates for all nodes
+        const dbids = await this.findLeafNodes(this.viewer.model);
+        this._panel.update(this.viewer.model, dbids, SUMMARY_PROPS);
+      }
+    }
+  }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension(
+  "SummaryExtension",
+  SummaryExtension
+);
---
+++ wwwroot/viewer.js
@@ -0,0 +1,52 @@
+/// import * as Autodesk from "@types/forge-viewer";
+
+import "./extensions/LoggerExtension.js";
+import "./extensions/SummaryExtension.js";
+import "./extensions/HistogramExtension.js";
+import "./extensions/DataGridExtension.js";
+
+async function getAccessToken(callback) {
+  try {
+    const resp = await fetch("/api/auth/token");
+    if (!resp.ok) throw new Error(await resp.text());
+    const { access_token, expires_in } = await resp.json();
+    callback(access_token, expires_in);
+  } catch (err) {
+    alert("Could not obtain access token. See the console for more details.");
+    console.error(err);
+  }
+}
+
+export function initViewer(container) {
+  return new Promise(function (resolve, reject) {
+    Autodesk.Viewing.Initializer({ getAccessToken }, async function () {
+      const config = {
+        extensions: [
+          "LoggerExtension",
+          "SummaryExtension",
+          "HistogramExtension",
+          "DataGridExtension",
+        ],
+      };
+      const viewer = new Autodesk.Viewing.GuiViewer3D(container, config);
+      viewer.start();
+      viewer.setTheme("light-theme");
+      resolve(viewer);
+    });
+  });
+}
+
+export function loadModel(viewer, urn) {
+  function onDocumentLoadSuccess(doc) {
+    viewer.loadDocumentNode(doc, doc.getRoot().getDefaultGeometry());
+  }
+  function onDocumentLoadFailure(code, message) {
+    alert("Could not load model. See console for more details.");
+    console.error(message);
+  }
+  Autodesk.Viewing.Document.load(
+    "urn:" + urn,
+    onDocumentLoadSuccess,
+    onDocumentLoadFailure
+  );
+}
